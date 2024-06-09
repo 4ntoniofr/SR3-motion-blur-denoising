1dd139b71697983564e7bb998c0cb0ab2592e672
4ntoniofr <afr012240@uma.es>
2024-06-09 15:27:02 +0200

    add setup + train wizard

---
+++ setup.py
@@ -0,0 +1,150 @@
+import requests
+from tqdm import tqdm
+import os
+import nibabel as nib
+from PIL import Image
+import numpy as np
+import zipfile
+import random
+
+
+def descargar_dataset_CAMUS():
+    url = 'https://humanheart-project.creatis.insa-lyon.fr/database/api/v1/folder/63fde55f73e9f004868fb7ac/download'
+    response = requests.get(url, stream=True)
+
+    # Verificar si la descarga fue exitosa
+    if response.status_code == 200:
+        # Crear una barra de carga en porcentaje
+        total_size = int(response.headers.get('content-length', 0))
+        block_size = 1024 * 1024  # 1 MB
+        with open('camus-dataset.zip', 'wb') as file:
+            for data in tqdm(response.iter_content(block_size), total=total_size // block_size, unit='kB',
+                             unit_scale=True):
+                file.write(data)
+        print('¡El conjunto de datos CAMUS ha sido descargado exitosamente!')
+    else:
+        print(f"Error al descargar el conjunto de datos CAMUS. Código de estado: {response.status_code}")
+
+
+def extraer_dataset_CAMUS(path_to_zip_file='camus-dataset.zip', directory_to_extract_to='./CAMUS_dataset/'):
+    with zipfile.ZipFile(path_to_zip_file, 'r') as zip_ref:
+        for file in tqdm(zip_ref.namelist(), desc="Extrayendo conjunto de datos CAMUS", unit="archivos"):
+            zip_ref.extract(file, directory_to_extract_to)
+
+
+def listar_archivos_nii_recursivamente(directory):
+    nii_files = []
+    for root, _, files in os.walk(directory):
+        for file in files:
+            if file.endswith(".nii.gz") and not file.endswith("_gt.nii.gz"):
+                nii_files.append(os.path.join(root, file))
+    return nii_files
+
+
+def NIB_a_PNG(input_dir, train_percent=0.6, val_percent=0.2, test_percent=0.2, train_dir="./SR3/dataset/train_camus",
+              val_dir="./SR3/dataset/validation_camus",
+              test_dir="./SR3/dataset/test_camus"):
+    # Crear directorios si no existen
+    os.makedirs(train_dir, exist_ok=True)
+    os.makedirs(os.path.join(train_dir, "hr_128"), exist_ok=True)
+    os.makedirs(os.path.join(train_dir, "lr_128"), exist_ok=True)
+    os.makedirs(os.path.join(train_dir, "sr_128_128"), exist_ok=True)
+
+    os.makedirs(val_dir, exist_ok=True)
+    os.makedirs(os.path.join(val_dir, "hr_128"), exist_ok=True)
+    os.makedirs(os.path.join(val_dir, "lr_128"), exist_ok=True)
+    os.makedirs(os.path.join(val_dir, "sr_128_128"), exist_ok=True)
+
+    os.makedirs(test_dir, exist_ok=True)
+    os.makedirs(os.path.join(test_dir, "hr_128"), exist_ok=True)
+    os.makedirs(os.path.join(test_dir, "lr_128"), exist_ok=True)
+    os.makedirs(os.path.join(test_dir, "sr_128_128"), exist_ok=True)
+
+    # Listar recursivamente todos los archivos nii.gz en el directorio de entrada, excluyendo los que terminan en _gt.nii.gz
+    files = listar_archivos_nii_recursivamente(input_dir)
+
+    # Barajar los archivos
+    random.shuffle(files)
+
+    # Calcular el número de archivos para cada conjunto
+    total_files = len(files)
+    train_count = int(total_files * train_percent)
+    val_count = int(total_files * val_percent)
+    test_count = total_files - train_count - val_count
+
+    # Dividir los archivos en conjuntos de entrenamiento, validación y prueba
+    train_files = files[:train_count]
+    val_files = files[train_count:train_count + val_count]
+    test_files = files[train_count + val_count:]
+
+    def procesar_y_guardar(file, output_dir, size):
+        try:
+            raw_img = nib.load(file)
+            array = raw_img.get_fdata()
+        except Exception as e:
+            return
+
+        if len(array.shape) == 3:
+            for i in range(array.shape[-1]):
+                image = Image.fromarray(np.uint8(array[:, :, i])).rotate(-90, expand=True).resize(size)
+                filename = os.path.basename(file)[:-7] + "-" + str(i + 1) + ".png"
+                image.save(os.path.join(output_dir, os.path.join("hr_128", filename)))
+                image.save(os.path.join(output_dir, os.path.join("lr_128", filename)))
+                image.save(os.path.join(output_dir, os.path.join("sr_128_128", filename)))
+        elif len(array.shape) == 2:
+            image = Image.fromarray(np.uint8(array)).rotate(-90, expand=True).resize(size)
+            filename = os.path.basename(file)[:-7] + ".png"
+            image.save(os.path.join(output_dir, os.path.join("hr_128", filename)))
+            image.save(os.path.join(output_dir, os.path.join("lr_128", filename)))
+            image.save(os.path.join(output_dir, os.path.join("sr_128_128", filename)))
+        else:
+            print(f"Error en la imagen {file}")
+
+    # Procesar y guardar archivos en los directorios correspondientes
+    for file in tqdm(train_files, desc="Procesando archivos de entrenamiento", unit="archivos"):
+        procesar_y_guardar(file, train_dir, (128, 128))
+
+    for file in tqdm(val_files, desc="Procesando archivos de validación", unit="archivos"):
+        procesar_y_guardar(file, val_dir, (128, 128))
+
+    for file in tqdm(test_files, desc="Procesando archivos de prueba", unit="archivos"):
+        procesar_y_guardar(file, test_dir, (128, 128))
+
+
+def main():
+    already_downloaded = input("¿Ya descargó el conjunto de datos CAMUS? (s/N): ") or 'N'
+    if already_downloaded.lower() != 's':
+        print("Descargando conjunto de datos CAMUS...")
+        descargar_dataset_CAMUS()
+        print("Extrayendo conjunto de datos CAMUS...")
+        extraer_dataset_CAMUS()
+    else:
+        input_dir = input(
+            "Ingrese el directorio donde se encuentra el conjunto de datos CAMUS extraído (predeterminado: './CAMUS_dataset/'): ") or './CAMUS_dataset/'
+
+    train_dir = input(
+        "Ingrese el directorio para guardar imágenes de entrenamiento (predeterminado: './SR3/dataset/train_camus'): ") or './SR3/dataset/train_camus'
+    val_dir = input(
+        "Ingrese el directorio para guardar imágenes de validación (predeterminado: './SR3/dataset/validation_camus'): ") or './SR3/dataset/validation_camus'
+    test_dir = input(
+        "Ingrese el directorio para guardar imágenes de prueba (predeterminado: './SR3/dataset/test_camus'): ") or './SR3/dataset/test_camus'
+    train_percent = input("Ingrese el porcentaje de datos para entrenamiento (predeterminado: 0.6): ") or 0.6
+    val_percent = input("Ingrese el porcentaje de datos para validación (predeterminado: 0.2): ") or 0.2
+    test_percent = input("Ingrese el porcentaje de datos para prueba (predeterminado: 0.2): ") or 0.2
+
+    # Asegurarse de que los porcentajes sumen 1
+    train_percent = float(train_percent)
+    val_percent = float(val_percent)
+    test_percent = float(test_percent)
+    if train_percent + val_percent + test_percent != 1.0:
+        print("Error: La suma de los porcentajes de entrenamiento, validación y prueba debe ser 1.0")
+        return
+
+    print("Convirtiendo imágenes NIfTI a PNG...")
+    NIB_a_PNG(input_dir, train_percent, val_percent, test_percent, train_dir, val_dir, test_dir)
+
+    print("¡Procesamiento completado!")
+
+
+if __name__ == "__main__":
+    main()
---
+++ train_wizard.py
@@ -0,0 +1,59 @@
+import json
+import os
+import subprocess
+
+
+def modificar_JSON_configuracion(n_iter=600000, val_freq=5000, save_freq=1000, print_freq=10, resume_state="",
+                                 train_dataset='./SR3/dataset/train_camus',
+                                 val_dataset='./SR3/dataset/validation_camus', resolution=128):
+    json_file = str(os.path.join("SR3", os.path.join("config", "train_wizard_deblurring.json")))
+
+    with open(json_file, 'r') as file:
+        data = json.load(file)
+
+    data["train"]["n_iter"] = n_iter
+    data["train"]["val_freq"] = val_freq
+    data["train"]["save_checkpoint_freq"] = save_freq
+    data["train"]["print_freq"] = print_freq
+    data["path"]["resume_state"] = None if resume_state == "" else resume_state
+    data["datasets"]["train"]["dataroot"] = train_dataset
+    data["datasets"]["val"]["dataroot"] = val_dataset
+    data["datasets"]["train"]["l_resolution"] = resolution
+    data["datasets"]["train"]["r_resolution"] = resolution
+    data["datasets"]["val"]["l_resolution"] = resolution
+    data["datasets"]["val"]["r_resolution"] = resolution
+    data["model"]["diffusion"]["image_size"] = resolution
+
+    with open(json_file, 'w') as file:
+        json.dump(data, file, indent=4)
+
+
+def entrenar_SR3():
+    command = "python " + os.path.join("SR3", "sr.py") + " --config " + os.path.join("config", "train_wizard_deblurring.json") + " -p train"
+    process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE)
+    process.wait()
+    print(process.stdout.read().decode("utf-8"))
+
+
+def main():
+    resume_state = input(
+        "Ingrese la ruta del punto de control de entrenamiento para reanudar (predeterminado: ninguno): ") or ""
+    train_dataset = input(
+        "Ingrese el directorio para guardar imágenes de entrenamiento (predeterminado: './SR3/dataset/train_camus'): ") or './SR3/dataset/train_camus'
+    val_dataset = input(
+        "Ingrese el directorio para guardar imágenes de validación (predeterminado: './SR3/dataset/validation_camus'): ") or './SR3/dataset/validation_camus'
+    resolution = input("Ingrese la resolución de las imágenes (predeterminado: 128): ") or 128
+    n_iter = input(
+        "Ingrese el número de iteraciones durante las que el modelo entrenará (predeterminado: 600000): ") or 600000
+    val_freq = input("Ingrese la frecuencia de validación en iteraciones (predeterminado: 5000): ") or 5000
+    save_freq = input(
+        "Ingrese la frecuencia de guardado de puntos de control en iteraciones (predeterminado: 1000): ") or 1000
+    print_freq = input("Ingrese la frecuencia de impresión de resultados en iteraciones (predeterminado: 10): ") or 10
+
+    modificar_JSON_configuracion(n_iter, val_freq, save_freq, print_freq, resume_state, train_dataset, val_dataset,
+                                 resolution)
+    entrenar_SR3()
+
+
+if __name__ == "__main__":
+    main()
