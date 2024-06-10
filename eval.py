935b0b5107398ed4e58c15c59df9c8d319f93040
4ntoniofr <afr012240@uma.es>
2024-06-10 14:01:06 +0200

    Optimizing eval.py

--- eval.py
+++ eval.py
@@ -21,19 +21,18 @@ def calculate_ssim(image1_path, image2_path):
 
 def calculate_mean_ssim(directory_path):
     ssim_values = {}
-    for directory in os.listdir(directory_path):
-        for filename in os.listdir(os.path.join(directory_path, directory)):
-            if filename.endswith("_hr.png"):
-                hr_path = os.path.join(directory_path, directory, filename)
-                sr_filename = filename.replace("_hr.png", "_sr.png")
-                sr_path = os.path.join(directory_path, directory, sr_filename)
-                if os.path.exists(sr_path):
-                    x_value = filename.split("_")[0]
-                    if x_value not in ssim_values:
-                        ssim_values[x_value] = []
-                    ssim_value = calculate_ssim(hr_path, sr_path)
-                    if ssim_value is not None:
-                        ssim_values[x_value].append(ssim_value)
+    for filename in os.listdir(directory_path):
+        if filename.endswith("_hr.png"):
+            hr_path = os.path.join(directory_path, filename)
+            sr_filename = filename.replace("_hr.png", "_sr.png")
+            sr_path = os.path.join(directory_path, sr_filename)
+            if os.path.exists(sr_path):
+                x_value = filename.split("_")[0]
+                if x_value not in ssim_values:
+                    ssim_values[x_value] = []
+                ssim_value = calculate_ssim(hr_path, sr_path)
+                if ssim_value is not None:
+                    ssim_values[x_value].append(ssim_value)
 
     mean_ssim_values = {x: sum(ssim_list) / len(ssim_list) for x, ssim_list in ssim_values.items()}
     ssim_df = pd.DataFrame(mean_ssim_values.items(), columns=["X", "Mean SSIM"])
@@ -56,19 +55,18 @@ def calculate_psnr(image1_path, image2_path):
 
 def calculate_mean_psnr(directory_path):
     psnr_values = {}
-    for directory in os.listdir(directory_path):
-        for filename in os.listdir(os.path.join(directory_path, directory)):
-            if filename.endswith("_hr.png"):
-                hr_path = os.path.join(directory_path, directory, filename)
-                sr_filename = filename.replace("_hr.png", "_sr.png")
-                sr_path = os.path.join(directory_path, directory, sr_filename)
-                if os.path.exists(sr_path):
-                    x_value = filename.split("_")[0]
-                    if x_value not in psnr_values:
-                        psnr_values[x_value] = []
-                    psnr_value = calculate_psnr(hr_path, sr_path)
-                    if psnr_value is not None:
-                        psnr_values[x_value].append(psnr_value)
+    for filename in os.listdir(directory_path):
+        if filename.endswith("_hr.png"):
+            hr_path = os.path.join(directory_path, filename)
+            sr_filename = filename.replace("_hr.png", "_sr.png")
+            sr_path = os.path.join(directory_path, sr_filename)
+            if os.path.exists(sr_path):
+                x_value = filename.split("_")[0]
+                if x_value not in psnr_values:
+                    psnr_values[x_value] = []
+                psnr_value = calculate_psnr(hr_path, sr_path)
+                if psnr_value is not None:
+                    psnr_values[x_value].append(psnr_value)
 
     mean_psnr_values = {x: sum(psnr_list) / len(psnr_list) for x, psnr_list in psnr_values.items()}
     psnr_df = pd.DataFrame(mean_psnr_values.items(), columns=["X", "Mean PSNR"])
@@ -79,68 +77,85 @@ def calculate_mean_psnr(directory_path):
     return psnr_df
 
 
-def obtener_metricas(input_directory, output_directory):
+def obtener_metricas(input_directory, output_directory, resolution=128):
+    print("Obteniendo metricas...")
+    input_directory = os.path.join(input_directory, "hr_" + str(resolution))
+    output_directory = os.path.join(output_directory, "results")
+    while len(os.listdir(output_directory)) == 0:
+        time.sleep(2)
+        os.system("clear")
+        print("Esperando a que se procesen las imágenes...")
     while len(os.listdir(output_directory)) * 5 < len(os.listdir(input_directory)):
-        time.sleep(30)
+        time.sleep(5)
         os.system("clear")
         mean_ssim = calculate_mean_ssim(output_directory)
         mean_psnr = calculate_mean_psnr(output_directory)
-        print(f"{len(os.listdir(output_directory)) * 5 / len(os.listdir(input_directory)) * 100}% of images processed")
-        print(f"Mean SSIM values: {mean_ssim}")
-        print(f"Mean PSNR values: {mean_psnr}")
+        processed_percentage = len(os.listdir(output_directory)) * 5 / len(os.listdir(input_directory)) * 100
+        print(f"Directorio de resultados: {output_directory}")
+        print(f"{processed_percentage:.4g}% de las imágenes procesadas")
+        mean_ssim_value = float(mean_ssim['Mean SSIM'].iloc[0])
+        mean_psnr_value = float(mean_psnr['Mean PSNR'].iloc[0])
+        print(f"Media de SSIM: {mean_ssim_value:.4f}")
+        print(f"Media de PSNR: {mean_psnr_value:.4f}")
 
 
 def evaluar_SR3(config_file="eval_deblurring.json"):
-    command = "python " + os.path.join("SR3", "sr.py") + " --config " + os.path.join("config",
-                                                                                     config_file) + " -p eval"
-    process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE)
+    command = "python " + os.path.join("SR3", "sr.py") + " --config " + os.path.join("SR3", "config",
+                                                                                     config_file) + " -p val"
+    process = subprocess.Popen(command, shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
     process.wait()
-    print(process.stdout.read().decode("utf-8"))
 
 
 def modificar_JSON_configuracion(config_file="eval_deblurring.json",
                                  pretrained_model="/SR3/pretrained_models/pretrained_motion_blur_camus/I580000_E180",
-                                 input_directory="/SR3/validation_camus"):
-    json_file = str(os.path.join("SR3", os.path.join("config", config_file)))
+                                 input_directory="/SR3/dataset/test_camus",
+                                 resolution=128):
+    json_file = str(os.path.join("SR3", "config", config_file))
 
     with open(json_file, 'r') as file:
         data = json.load(file)
 
     data["path"]["resume_state"] = pretrained_model
     data["datasets"]["val"]["dataroot"] = input_directory
+    data["datasets"]["val"]["l_resolution"] = resolution
+    data["datasets"]["val"]["r_resolution"] = resolution
 
     with open(json_file, 'w') as file:
         json.dump(data, file, indent=4)
 
 
 def main():
-    config_file = input(
-        "Ingrese el nombre del archivo de configuración (predeterminado: eval_deblurring.json): ") or "eval_deblurring.json"
+    config_file = "eval_deblurring.json"
     pretrained_model = input(
-        "Ingrese la ruta del modelo preentrenado (predeterminado: /SR3/pretrained_models/pretrained_motion_blur_camus/I580000_E180): ") or "/SR3/pretrained_models/pretrained_motion_blur_camus/I580000_E180"
+        "Ingrese la ruta del modelo preentrenado (predeterminado: ./SR3/pretrained_models/pretrained_motion_blur_camus/I580000_E180): ") or "./SR3/pretrained_models/pretrained_motion_blur_camus/I580000_E180"
     input_directory = input(
-        "Ingrese el conjunto de datos a evaluar en la carpeta SR3/datasets (predeterminado: /SR3/validation_camus): ") or "/SR3/validation_camus"
+        "Ingrese el conjunto de datos a evaluar en la carpeta SR3/datasets (predeterminado: ./SR3/dataset/test_camus): ") or "./SR3/dataset/test_camus"
 
-    modificar_JSON_configuracion(config_file, pretrained_model, input_directory)
+    resolution_input = input("Ingrese la resolución de las imágenes (predeterminado: 128): ")
+    resolution = int(resolution_input) if resolution_input else 128
 
-    os.makedirs("experiments", exist_ok=True)
-    experiments_before = os.listdir("experiments")
+    modificar_JSON_configuracion(config_file, pretrained_model, input_directory, resolution)
 
-    process_1 = Process(target=evaluar_SR3, args=config_file)
+    os.makedirs("./experiments", exist_ok=True)
+    experiments_before = os.listdir("./experiments")
+
+    process_1 = Process(target=evaluar_SR3)
     process_1.start()
-    process_1.join()
 
-    experiments_after = os.listdir("experiments")
-    experiment_directory = list(set(experiments_after) - set(experiments_before))[0]
+    experiments_after = os.listdir("./experiments")
+    experiment_directory = list(set(experiments_after) - set(experiments_before))
 
-    while experiment_directory is None:
-        time.sleep(30)
-        experiments_after = os.listdir("experiments")
-        experiment_directory = list(set(experiments_after) - set(experiments_before))[0]
+    while len(experiment_directory) == 0:
+        time.sleep(2)
+        experiments_after = os.listdir("./experiments")
+        experiment_directory = list(set(experiments_after) - set(experiments_before))
 
-    process_2 = Process(target=obtener_metricas, args=(
-        os.path.join("SR3", "dataset", input_directory), os.path.join("SR3", "results", experiment_directory)))
+    experiment_directory = experiment_directory[0]
+    process_2 = Process(target=obtener_metricas,
+                        args=(input_directory, os.path.join("./experiments", experiment_directory)))
     process_2.start()
+
+    process_1.join()
     process_2.join()
 
 
